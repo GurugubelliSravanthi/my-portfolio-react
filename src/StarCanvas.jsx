c4e128361529c746cf9712c42b1b0e1ec6cf5e45
Sravanthi Gurugubelli <sravanthigurgubelli@gmail.com>
2025-05-31 02:33:52 +0530

    Add files via upload

---
+++ src/StarCanvas.jsx
@@ -0,0 +1,48 @@
+import React, { useRef, useState, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { inSphere } from "maath/random";
+
+const StarBackground = () => {
+  const ref = useRef();
+
+  // Generate valid sphere points
+  const [positions] = useState(() => {
+    const points = inSphere(new Float32Array(5000 * 3), { radius: 1.5 });
+    return points;
+  });
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={positions} stride={3} frustumCulled>
+        <PointMaterial
+          transparent
+          color="#ffffff"
+          size={0.005}
+          sizeAttenuation
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => (
+  <div className="star-background">
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  </div>
+);
+
+export default StarsCanvas;
